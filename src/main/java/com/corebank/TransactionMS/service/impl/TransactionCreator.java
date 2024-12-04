f7548c5b31d11cfb42c93322bdb580b55468eaf9
yulychr <sandra_ch.r@hotmail.com>
2024-12-04 01:28:16 -0500

    fix: remove @Autowired from unnecessary field

--- src/main/java/com/corebank/TransactionMS/service/impl/TransactionCreator.java
+++ src/main/java/com/corebank/TransactionMS/service/impl/TransactionCreator.java
@@ -18,9 +18,6 @@ public class TransactionCreator {
         this.transactionRepository = transactionRepository;
     }
 
-    @Autowired
-
-
     public Mono<Transaction> createTransaction(String type, double amount, String sourceAccount, String destinationAccount) {
         Transaction transaction = new Transaction();
         transaction.setDate(LocalDateTime.now());
